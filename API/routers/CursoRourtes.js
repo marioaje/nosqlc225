55481cf5f3a02ffa50bd172d763792165c4d1d5a
marioaje <marioaje@gmail.com>
2025-07-03 13:09:00 -0600

    Update CursoRourtes.js

--- API/routers/CursoRourtes.js
+++ API/routers/CursoRourtes.js
@@ -25,6 +25,44 @@ router.get('/', async(req, res) =>{
 
     }
 );
+//Obtener 
+router.get('/:id', async(req, res) =>{
+        const listaDatos = await Curso.findOne({id: req.params.id});
+        if (listaDatos) {
+            res.json(listaDatos);
+        }
+        else{
+            res.status(404).json({error: "No se encontro el elemento"});
+        }
+    }
+);
+
+
+//Actualizar 
+router.put('/:id', async(req, res) =>{
+        const dato = await Curso.findOneAndUpdate({
+                id: req.params.id}, req.body, {new: true                
+            });
+        if (dato){
+            res.json(dato);
+        }
+        else{
+            res.status(404).json({error: "No se encontro el elemento para actualizar"});
+        }
+    }
+);
+
+//Obtener 
+router.delete('/:id', async(req, res) =>{
+        const dato = await Curso.findByIdAndDelete({id: req.params.id});
+        if (dato) {
+            res.status(200).json({mensaje: "El elemento fue eliminado"});
+        }
+        else{
+            res.status(404).json({error: "No se encontro el elemento"});
+        }
+    }
+);
 
 
 module.exports = router;
